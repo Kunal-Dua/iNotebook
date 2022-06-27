dcc26a3c8cdf1df2f44d00daae35d050ccf3ee8e
Kunal Dua <kunaldua2000@gmail.com>
2022-06-27 13:26:16 +0530

    modules added

---
+++ backend/modules/Notes.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    tag:{
+        type:String
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+});
+
+module.exports=mongoose.model('notes',NotesSchema);
