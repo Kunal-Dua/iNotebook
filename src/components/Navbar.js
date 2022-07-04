3ffcb5ba9992be10d5b1030759da3a4d4e91bfee
Kunal Dua <kunaldua2000@gmail.com>
2022-07-05 00:53:01 +0530

    basic config

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+export default function Navbar(props) {
+    return (
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">{props.title}</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        {/* <li class="nav-item">
+                            <Link class="nav-link" href="/about">About</Link>
+                        </li> */}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
