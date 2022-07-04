8b5ead76da969921cefa3aa9921c5faead80fd1a
Kunal Dua <kunaldua2000@gmail.com>
2022-07-04 22:18:05 +0530

    update route endpint added

--- backend/Routes/notes.js
+++ backend/Routes/notes.js
@@ -10,16 +10,13 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
     res.json(notes);
 })
 
-//ROUTE 2 add new node using post Login required
+//ROUTE 2 add new note using post Login required
 router.get('/addnote', fetchuser, [
     body('title', "Enter a valid title").isLength({ min: 3 }),
     body('description', "Enter a valid description").isLength({ min: 5 })
 ], async (req, res) => {
     try {
         const { title, description, tag } = req.body;
-
-        // const notes = await Note.find({ user: req.user.id });
-        // res.json(notes);
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
@@ -29,6 +26,33 @@ router.get('/addnote', fetchuser, [
         })
         const savedNote = await note.save();
         res.json(savedNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+})
+
+//ROUTE 3 update note using put Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        //create new node
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        let note = await Note.findById(req.params.id);
+
+        if (!note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(404).send("Not Allowed")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({note});
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal server error");
