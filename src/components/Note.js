e3fc6a46a5398c1f57826ca22fc143a765dc11a1
Kunal Dua <kunaldua2000@gmail.com>
2022-07-10 05:41:19 +0530

    modal added

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,22 +1,51 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import noteContext from "../context/notes/noteContext";
 import AddNote from "./AddNote";
 import NoteItem from "./NoteItem";
 
 const Note = () => {
     const context = useContext(noteContext);
-    const { note,getNotes } = context;
+    const { note, getNotes } = context;
+    const ref = useRef(null);
+    const updateNote=()=>{
+        ref.current.click();
+    }
     useEffect(() => {
-      getNotes();
+        getNotes();
     }, [])
-    
+
     return (
         <>
             <AddNote />
             <div className="row my-3">
+
+                {/* <!-- Button trigger modal --> */}
+                <button type="button" ref={ref} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+                </button>
+
+                {/* <!-- Modal --> */}
+                <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Edit note</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                ...
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <h2>Your Notes</h2>
                 {note.map((Note) => {
-                    return <NoteItem key={Note._id} note={Note} />
+                    return <NoteItem key={Note._id} updateNote={updateNote} note={Note} />
                 })}
             </div>
         </>
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -3,7 +3,7 @@ import noteContext from "../context/notes/noteContext";
 
 const NoteItem = (props) => {
     const context = useContext(noteContext);
-    const { note } = props;
+    const { note,updateNote } = props;
     const { deleteNote } = context;
     return (
         <div className="col-md-3">
@@ -11,7 +11,7 @@ const NoteItem = (props) => {
                 <div className="card-body">
                     <div className="d-flex align-items-center">
                         <h5 className="card-title">{note.title}</h5>
-                        <i className="fa-solid fa-file-pen mx-2 "></i>
+                        <i className="fa-solid fa-file-pen mx-2 " onClick={()=>{updateNote(note)}}></i>
                         <i className="fa-solid fa-trash" onClick={()=>{deleteNote(note._id)}}></i>
                     </div>
                     <p className="card-text">{note.description}</p>
